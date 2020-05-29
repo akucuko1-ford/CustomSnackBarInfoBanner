9a0aab1b801f3f656603cda25bc2297ee16bdd17
Kucukoz <akucuko1@MGC12Z6498LVCG.fbpld77.ford.com>
2020-05-29 00:17:42 -0400

    slide out animation is added

--- app/src/main/java/com/google/android/material/snackbar/BaseTransientTopBar.kt
+++ app/src/main/java/com/google/android/material/snackbar/BaseTransientTopBar.kt
@@ -20,11 +20,19 @@ abstract class BaseTransientTopBar<T : BaseTransientTopBar<T>>(
     override fun animateViewIn() {
         view.post {
             view.visibility = View.VISIBLE
-            startSlideAnimation()
+            startSlideInAnimation()
         }
     }
 
-    private fun startSlideAnimation() {
+    override fun onViewHidden(event: Int) {
+        if (view.visibility == View.VISIBLE && event != BaseCallback.DISMISS_EVENT_MANUAL) {
+            startSlideOutAnimation(event)
+        } else {
+            super.onViewHidden(event)
+        }
+    }
+
+    private fun startSlideInAnimation() {
         val translationYBottom = view.height * -1f
         view.translationY = translationYBottom
         ValueAnimator().apply {
@@ -33,12 +41,12 @@ abstract class BaseTransientTopBar<T : BaseTransientTopBar<T>>(
             duration = 250
             addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationStart(animation: Animator?) {
+                    shouldAnimate = false
                     contentViewCallback.animateContentIn(70, 180)
                 }
 
                 override fun onAnimationEnd(animation: Animator?) {
                     onViewShown()
-                    shouldAnimate = false
                 }
             })
             addUpdateListener {
@@ -46,5 +54,32 @@ abstract class BaseTransientTopBar<T : BaseTransientTopBar<T>>(
             }
             start()
         }
+    }
+
+    private fun startSlideOutAnimation(event: Int) {
+        val translationYBottom = view.height * -1f
+        view.translationY = translationYBottom
+        ValueAnimator().apply {
+            setFloatValues(0f, translationYBottom)
+            interpolator = FastOutSlowInInterpolator()
+            duration = 250
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator?) {
+                    contentViewCallback.animateContentOut(0, ANIMATION_FADE_DURATION)
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    removeView(event)
+                }
+            })
+            addUpdateListener {
+                view.translationY = it.animatedValue as Float
+            }
+            start()
+        }
+    }
+
+    private fun removeView(event: Int) {
+        super.onViewHidden(event)
     }
 }
